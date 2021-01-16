ee430a265c863a91cb78e75d6f85e902c8146e4d
Artsiom <pchelenkovartem@gmail.com>
2021-01-16 19:04:01 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const toggleMenu = document.querySelector(".toggleMenu");
+    const navigation = document.querySelector(".nav-items");
+    toggleMenu.classList.toggle("active");
+    navigation.classList.toggle("active");
+}
+
